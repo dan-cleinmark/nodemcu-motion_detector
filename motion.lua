15fdc3a1d60db693c93e40e4507c2f2b44d71ad9
Dan Cleinmark <dan.cleinmark@gmail.com>
2015-09-03 18:21:03 -0500

    use tmr.time() to calculate duration of motion

--- motion.lua
+++ motion.lua
@@ -1,46 +1,20 @@
 inpin=6
 gpio.mode(inpin, gpio.INT)
 
-counter=0
-datetime="nope"
-
+start=tmr.time()
 
 function motionStart()
-  now=getDateTime()
-  print("Motion detected at "..now)
+  start=tmr.time()
+  print('Motion detected!')
   tmr.delay(1000000)
   gpio.trig(inpin, "down", motionStop)
 end
 
 function motionStop()
-  now=getDateTime()
-  print("Motion stopped at "..now)
+  duration = tmr.time() - start
+  print('motion ended after '..duration..' seconds.')
   tmr.delay(1000000)
   gpio.trig(inpin, "up", motionStart)
 end
-
-function getDateTime()
-  conn=net.createConnection(net.TCP, 0)
-  conn:on("connection",function(conn, payload)
-            conn:send("HEAD / HTTP/1.1\r\n".. 
-                      "Host: google.com\r\n"..
-                      "Accept: */*\r\n"..
-                      "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua;)"..
-                      "\r\n\r\n") 
-            end)
-            
-  conn:on("receive", function(conn, payload)
-    datetime=string.sub(payload,string.find(payload,"Date: ")
-            +6,string.find(payload,"Date: ")+35)
-    print('\nRetrieved in '..((tmr.now()-t)/1000)..' milliseconds.')
-    print('Google says it is '..datetime)
-    conn:close()
-    end) 
-  
-  t = tmr.now()    
-  conn:connect(80,'google.com') 
-  return datetime
-end
-  
 
 gpio.trig(inpin, "up", motionStart)
