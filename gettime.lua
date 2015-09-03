4c6d101accddea40ba260280d0c63e6e2f9583c6
Dan Cleinmark <dan.cleinmark@climate.com>
2015-09-03 15:06:15 -0700

    add logic to pull the current time from google

---
+++ gettime.lua
@@ -0,0 +1,18 @@
+conn=net.createConnection(net.TCP, 0) 
+
+conn:on("connection",function(conn, payload)
+            conn:send("HEAD / HTTP/1.1\r\n".. 
+                      "Host: google.com\r\n"..
+                      "Accept: */*\r\n"..
+                      "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua;)"..
+                      "\r\n\r\n") 
+            end)
+            
+conn:on("receive", function(conn, payload)
+    print('\nRetrieved in '..((tmr.now()-t)/1000)..' milliseconds.')
+    print('Google says it is '..string.sub(payload,string.find(payload,"Date: ")
+           +6,string.find(payload,"Date: ")+35))
+    conn:close()
+    end) 
+t = tmr.now()    
+conn:connect(80,'google.com') 
--- motion.lua
+++ motion.lua
@@ -1,9 +1,46 @@
 inpin=6
 gpio.mode(inpin, gpio.INT)
 
-function motion()
-  print("Motion detected!")
+counter=0
+datetime="nope"
+
+
+function motionStart()
+  now=getDateTime()
+  print("Motion detected at "..now)
   tmr.delay(1000000)
+  gpio.trig(inpin, "down", motionStop)
 end
 
-gpio.trig(inpin, "high", motion)
+function motionStop()
+  now=getDateTime()
+  print("Motion stopped at "..now)
+  tmr.delay(1000000)
+  gpio.trig(inpin, "up", motionStart)
+end
+
+function getDateTime()
+  conn=net.createConnection(net.TCP, 0)
+  conn:on("connection",function(conn, payload)
+            conn:send("HEAD / HTTP/1.1\r\n".. 
+                      "Host: google.com\r\n"..
+                      "Accept: */*\r\n"..
+                      "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua;)"..
+                      "\r\n\r\n") 
+            end)
+            
+  conn:on("receive", function(conn, payload)
+    datetime=string.sub(payload,string.find(payload,"Date: ")
+            +6,string.find(payload,"Date: ")+35)
+    print('\nRetrieved in '..((tmr.now()-t)/1000)..' milliseconds.')
+    print('Google says it is '..datetime)
+    conn:close()
+    end) 
+  
+  t = tmr.now()    
+  conn:connect(80,'google.com') 
+  return datetime
+end
+  
+
+gpio.trig(inpin, "up", motionStart)
