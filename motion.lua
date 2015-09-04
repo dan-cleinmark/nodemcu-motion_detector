af7cbac79471584fbb518359dd494683eaaf1cc8
Dan Cleinmark <dan.cleinmark@gmail.com>
2015-09-04 08:33:19 -0500

    add space in debug message

--- motion.lua
+++ motion.lua
@@ -2,10 +2,13 @@ inpin=6
 gpio.mode(inpin, gpio.INT)
 
 start=tmr.time()
+lastEnd=0
 
 function motionStart()
   start=tmr.time()
   print('Motion detected!')
+  print('[DEBUG] start: '..start..', last: '..lastEnd..
+        ', break: '..start - lastEnd)
   tmr.delay(1000000)
   gpio.trig(inpin, "down", motionStop)
 end
@@ -13,7 +16,9 @@ end
 function motionStop()
   duration = tmr.time() - start
   print('motion ended after '..duration..' seconds.')
+  print('[DEBUG] start: '..start..', duration: '..duration)
   tmr.delay(1000000)
+  lastEnd=start + duration
   gpio.trig(inpin, "up", motionStart)
 end
 
